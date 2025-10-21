f8d16200434b336fa8c8d8930582057436b4da7e
Audrick <lunaraudrick26@gmail.com>
2025-10-21 21:52:54 +0400

    Completed Exercise 1: Coding is Cool

---
+++ 01-CodingIsCool.py
@@ -0,0 +1,9 @@
+# Exercise 1: Coding is Cool - 10 Marks
+# Fill in the blanks to output "Coding is Cool"
+
+word1 = "Coding"
+word2 = "is"
+word3 = "Cool"
+
+# Use string to combine the variables and print the phrase
+print(word1 + " " + word2 + " " + word3)
